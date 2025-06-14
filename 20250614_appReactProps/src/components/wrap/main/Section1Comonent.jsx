1f2c08b28654cf7449c4bf44c842c77be030d22c
Sang-Ho Lee <sangho.lee.1990@gmail.com>
2025-06-14 15:18:23 +0900

    c

--- 20250614_appReactProps/src/components/wrap/main/Section1Comonent.jsx
+++ 20250614_appReactProps/src/components/wrap/main/Section1Comonent.jsx
@@ -19,6 +19,58 @@ export default function Section1Comonent() {
             { type: 'image', src: './images/link3.jpg', alt: 'link3', title: 'link3' },
         ]
     })
+    
+    // 0. 카운트 상태변수 설정 초기값 0
+    // 리액트에서 제공하는 상태변수만 제어 함수
+    const [cnt, setCnt] = React.useState(0);
+
+    // 1. 메인슬라이드 1씩 카운트 함수
+    // const nextCount=()=>{
+    //     setCnt(cnt => cnt + 1);
+    // }
+
+    // 로딩시 실행하는 타이머 효과 적용 => 유즈이펙트 훅을 사용한다.
+    // 2. 타이머 사용 3초 간격으로 1씩 증가시킨다.
+    React.useEffect(() => {
+        const setId = setInterval(() => {
+            setCnt(cnt => cnt + 1);
+            // setCnt(cnt => cnt - 1);
+            return () => clearInterval(setId);
+        }, 3000)
+    }, [])
+
+    // 3. 리액트 선택자 슬라이드 랩퍼 박스 선택 정의
+    // document.querySelector('.slide-wrap');
+    // 리액트 DOM 선택자
+    // [1] 탬플릿에서 ref={slideWrap} => <ul ref={slideWrap} className="slide-wrap">
+    // [2] 상단에 등록 선언 useRef 훅 사용
+    const slideWrap = React.useRef();
+
+    // 4. 반드시 cnt가 1씩 증가하면 (변경되면) 와처 Watcher 감시자 => 의존성 변수 증가 시
+    // 슬라이드 우측에서 좌측으로 애니메이션 구현
+    React.useEffect(() => {
+        if (cnt > 3) {
+            // console.log('리턴 처음');
+            slideWrap.current.style.transition = 'left 0s';
+            slideWrap.current.style.left = `${-100 * 0}%`;
+            setTimeout(() => {
+                setCnt(1);
+            }, 100)
+        } else if (cnt < 0) {
+            // console.log(cnt);
+            // 선택자를 이용하여 애니메이션 구현
+            slideWrap.current.style.transition = 'left 0s';
+            slideWrap.current.style.left = `${-100 * 3}%`;
+            setTimeout(() => {
+                setCnt(2);
+            }, 100)
+        } else {
+            slideWrap.current.style.transition = 'left 0.6s ease-in-out';
+            slideWrap.current.style.left = `${-100 * cnt}%`;
+        }
+    }, [cnt])
+    
+
 
     return (
         // <section id="section1">
@@ -27,7 +79,8 @@ export default function Section1Comonent() {
         <section id="section1">
             <div className="slide-container">
                 <div className="slide-view">
-                    <ul className="slide-wrap">
+                    {/* 애니메이션 선택자 */}
+                    <ul ref={slideWrap} className="slide-wrap">
                         {/* <li className="slide slide3 last"><a href="!#" title="푸른마을 이미지 슬라이드3"><img src="./images/slide3.jpg" alt="푸른마을 이미지 슬라이드3"/><h2>푸른마을 이미지 슬라이드3</h2></a></li>
                         <li className="slide slide1"><a href="!#" title="푸른마을 이미지 슬라이드1"><img src="./images/slide1.jpg" alt="푸른마을 이미지 슬라이드1"/><h2>푸른마을 이미지 슬라이드1</h2></a></li>
                         <li className="slide slide2"><a href="!#" title="푸른마을 이미지 슬라이드2"><img src="./images/slide2.jpg" alt="푸른마을 이미지 슬라이드2"/><h2>푸른마을 이미지 슬라이드2</h2></a></li>
